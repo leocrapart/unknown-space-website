7ed7a67449f92b403bf461fb57159879fc69964d
Leo Crapart <leocrapart@yahoo.fr>
2021-09-16 15:31:00 +0200

    tailwind-svelte config

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,8 @@
+<script>
+  import "../styles/tailwind-output.css";
+</script>
+
+<h1 class="uppercase text-purple-600 font-bold">hello People of Earth</h1>
+<h2 class="font-semibold">Taste my haste</h2>
+
+<slot />
